1ccfa6341d792c7b2314c0bf433052de825bd6e8
בר אברמוביץ <barab2002@gmail.com>
2023-10-19 17:56:07 +0300

    create scrool ber

---
+++ TaskManagment/design/myList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using TaskManagment.classes;
+
+namespace TaskManagment.design
+{
+    internal class myList : Panel
+    {
+        DB dbHandler;
+
+        public myList(string query, Action<string> buttonFunc)
+        {
+            dbHandler = DB.Instance;
+            List<List<string>> dataFromDatabase = dbHandler.Query(query);
+
+            int verticalPosition = 150; // Initial vertical position
+
+            this.Dock = DockStyle.Right; // Fill the entire form
+            this.Width = 600;
+            this.Height = 600; // Limit the height of the main panel
+
+            // Create an inner container for the scrollable content
+            Panel innerPanel = new Panel();
+            innerPanel.AutoScroll = true;
+            innerPanel.Location = new Point(0, verticalPosition);
+            innerPanel.Width = this.Width;
+            innerPanel.Height = this.Height - verticalPosition;
+            this.Controls.Add(innerPanel);
+
+            verticalPosition = 0;
+
+            // Create a panel for each line of data and add it to the inner container
+            foreach (List<string> lineData in dataFromDatabase)
+            {
+                Panel linePanel = new RoundedPanel();
+                linePanel.BackColor = Color.FromArgb(216, 226, 235);
+                linePanel.Width = innerPanel.Width - 50;
+                linePanel.Height = 70;
+                linePanel.Location = new Point(10, verticalPosition);
+
+                Button button = new Button();
+                button.Text = string.Join(", ", lineData);
+                button.ForeColor = Color.FromArgb(136, 151, 170);
+                button.Font = new Font(button.Font.FontFamily, 32, FontStyle.Regular);
+                button.FlatAppearance.BorderSize = 0;
+                button.FlatStyle = FlatStyle.Flat;
+                button.TextAlign = ContentAlignment.MiddleCenter;
+                button.Dock = DockStyle.Fill;
+
+                button.Click += (sender, e) =>
+                {
+                    buttonFunc.Invoke(button.Text);
+                };
+
+                button.Parent = linePanel;
+                innerPanel.Controls.Add(linePanel);
+
+                verticalPosition += linePanel.Height + 10;
+            }
+        }
+    }
+}
--- TaskManagment/forms/listsForm.cs
+++ TaskManagment/forms/listsForm.cs
@@ -15,60 +15,30 @@ namespace TaskManagment.forms
 {
     public partial class listsForm : Form
     {
-        DB dbHandler;
         public listsForm(string username)
         {
-            dbHandler = DB.Instance;
+            string query1 = "select name from taskList where user = '" + username + "'";
             InitializeComponent();
             this.Controls.Add(new templatePanel(username));
-            List<List<string>> dataFromDatabase = dbHandler.Query("select name from taskList where user = '" + username + "'");
+            putTitle();
+            this.Controls.Add(new myList(query1, (taskListName) => {
+                string query2 = "SELECT name FROM task WHERE user = '" + username + "' AND taskListName = '" + taskListName + "'";
+                listPage listPage = new listPage(username, query2, taskListName);
+                this.Hide();
+                listPage.ShowDialog();
+            }));
+        }
 
-            // Create a Panel to contain the data panels
-            Panel dataPanel = new Panel();
-            dataPanel.Dock = DockStyle.Right; // Fill the entire form
-            dataPanel.Width = 600;
-            dataPanel.Height = 1000;
-            this.Controls.Add(dataPanel);
+        private void putTitle()
+        {
+            Label label = new Label();
+            label.AutoSize = true;
+            label.Text = "My Lists";
+            label.Font = new System.Drawing.Font(label.Font.FontFamily, 36, System.Drawing.FontStyle.Regular);
+            label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(151)))), ((int)(((byte)(170)))));
+            label.Location = new Point(this.Width - label.PreferredWidth - 250, 40);
 
-            int verticalPosition = 10; // Initial vertical position
-
-            // Create a panel for each line of data
-            foreach (List<string> lineData in dataFromDatabase)
-            {
-                // Create a panel for the current line of data
-                Panel linePanel = new RoundedPanel();
-                linePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(226)))), ((int)(((byte)(235)))));
-                linePanel.Width = dataPanel.Width - 20; // Adjust the width as needed
-                linePanel.Height = 70; // Set the height of the panel
-                linePanel.Location = new System.Drawing.Point(10, verticalPosition);
-
-                // Create a button to display the line data
-                Button button = new Button();
-                button.Text = string.Join(", ", lineData);
-                button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(151)))), ((int)(((byte)(170)))));
-                button.Font = new System.Drawing.Font(button.Font.FontFamily, 32, System.Drawing.FontStyle.Regular);
-                button.FlatAppearance.BorderSize = 0;
-                button.FlatStyle = FlatStyle.Flat;
-                button.TextAlign = ContentAlignment.MiddleCenter;
-                button.Dock = DockStyle.Fill;
-
-                button.Click += (sender, e) =>
-                {
-                    MessageBox.Show(string.Join(", ", lineData));
-                    string query = "SELECT name FROM task WHERE user = '" + username + "' AND taskListName = '" + string.Join(", ", lineData) + "'";
-                    listPage listPage = new listPage(username,query);
-                    this.Parent.Hide();
-                    listPage.ShowDialog();
-                };
-                // Set the label's Parent to the line panel
-                button.Parent = linePanel;
-
-                // Add the line panel to the data panel
-                dataPanel.Controls.Add(linePanel);
-
-                // Adjust the vertical position for the next panel
-                verticalPosition += linePanel.Height + 10; // Add 10 pixels of spacing between panels
-            }
+            this.Controls.Add(label);
         }
     }
 }
